25254ef40c5382db1f94da2a927fe7282fdd591c
sohei yamasaki <>
2023-01-07 16:56:19 +0900

    number picker導入

--- lib/pages/sugoroku_edit/sugoroku_edit_page.dart
+++ lib/pages/sugoroku_edit/sugoroku_edit_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class SugorokuEditPage extends StatelessWidget {
   const SugorokuEditPage({Key? key}) : super(key: key);
@@ -11,6 +13,23 @@ class SugorokuEditPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(hintText: '予定名'),
+                ),
+              ),
+              NumberPicker(
+                value: 0,
+                minValue: 0,
+                maxValue: 100,
+                axis: Axis.horizontal,
+                onChanged: (value) => (() => print(value)),
+              ),
+              const Text('時間'),
+            ],
+          ),
           const Text("sugoroku 編集 しようぜ"),
           TextButton(
               onPressed: () {
