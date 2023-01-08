3b521d9d7437441156fc0009b1f2793e03fec4f1
sohei yamasaki <>
2023-01-08 22:21:14 +0900

    とりあえずボタン類追加

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  // todo: あとでcolor.lightblueでアクセスできるようにしたい
+  static Color lightBlue = const Color.fromARGB(255, 134, 193, 245);
+}
--- lib/pages/sugoroku_edit/components/time_picker.dart
+++ lib/pages/sugoroku_edit/components/time_picker.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final _requiredTimeProvider = StateProvider<int>((ref) => 0);
+import '../../../constants.dart';
+
+final _requiredTimeProvider = StateProvider.autoDispose<int>((ref) => 0);
+// note: autoDispose修飾子を付けると画面がpopされたときステートも破棄される
 
 class TimePicker extends ConsumerWidget {
   const TimePicker({Key? key}) : super(key: key);
@@ -12,21 +15,69 @@ class TimePicker extends ConsumerWidget {
 
     return Column(
       children: [
+        // todo: hh:mm形式にする
         Text("$_requiredTime"),
+        // todo: 1hボタンの横幅長くしたい
+        ElevatedButton(
+          child: const Text('1h'),
+          style: ElevatedButton.styleFrom(
+            primary: Constants.lightBlue,
+            onPrimary: Colors.black,
+            shape: const StadiumBorder(),
+          ),
+          onPressed: () {
+            _addMinutes(ref, 60);
+          },
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('30min'),
+              style: ElevatedButton.styleFrom(
+                primary: Constants.lightBlue,
+                onPrimary: Colors.black,
+                shape: const StadiumBorder(),
+              ),
+              onPressed: () {
+                _addMinutes(ref, 30);
+              },
+            ),
+            // todo: 1hボタンの幅に連動するようにスペース上けたい
+            ElevatedButton(
+              child: const Text('15min'),
+              style: ElevatedButton.styleFrom(
+                primary: Constants.lightBlue,
+                onPrimary: Colors.black,
+                shape: const StadiumBorder(),
+              ),
+              onPressed: () {
+                _addMinutes(ref, 15);
+              },
+            ),
+          ],
+        ),
         TextButton(
           onPressed: () {
-            ref
-                .read(_requiredTimeProvider.notifier)
-                .update((state) => state + 1);
+            _addMinutes(ref, 1);
           },
           child: const Text('+'),
         ),
         TextButton(
             onPressed: () {
-              ref.read(_requiredTimeProvider.notifier).state = 0;
+              _clearMinutes(ref);
             },
             child: const Text('clear'))
       ],
     );
+  }
+
+  void _addMinutes(WidgetRef ref, int minutes) {
+    // todo: 24:00以上になったら24:00で止まるようにする
+    ref.read(_requiredTimeProvider.notifier).update((state) => state + minutes);
+  }
+
+  void _clearMinutes(WidgetRef ref) {
+    ref.read(_requiredTimeProvider.notifier).state = 0;
   }
 }
