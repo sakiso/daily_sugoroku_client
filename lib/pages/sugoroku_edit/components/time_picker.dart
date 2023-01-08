55c33a9d5f126cd8281934b36337dadd27aebba4
sohei yamasaki <>
2023-01-08 21:43:22 +0900

    clearボタン

--- lib/pages/sugoroku_edit/components/time_picker.dart
+++ lib/pages/sugoroku_edit/components/time_picker.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-StateProvider<int> _requiredTimeProvider = StateProvider((ref) => 0);
+final _requiredTimeProvider = StateProvider<int>((ref) => 0);
 
 class TimePicker extends ConsumerWidget {
   const TimePicker({Key? key}) : super(key: key);
@@ -10,6 +10,23 @@ class TimePicker extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     int _requiredTime = ref.watch(_requiredTimeProvider);
 
-    return Text("$_requiredTime");
+    return Column(
+      children: [
+        Text("$_requiredTime"),
+        TextButton(
+          onPressed: () {
+            ref
+                .read(_requiredTimeProvider.notifier)
+                .update((state) => state + 1);
+          },
+          child: const Text('+'),
+        ),
+        TextButton(
+            onPressed: () {
+              ref.read(_requiredTimeProvider.notifier).state = 0;
+            },
+            child: const Text('clear'))
+      ],
+    );
   }
 }
